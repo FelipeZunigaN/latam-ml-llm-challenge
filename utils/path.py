ef2c9cbc92a91067772eacea591eeb4b037657dd
FeZuniga <felipe.zuniga.norambuena@sqm.com>
2025-06-24 22:09:10 -0400

    fix: rename model path

--- utils/path.py
+++ utils/path.py
@@ -8,7 +8,7 @@ RAW_DATA_DIR = DATA_DIR / 'data.csv'
 
 # Ruta modelos
 MODEL_DIR = ROOT_DIR / 'models'
-XGB_MODEL_DIR = MODEL_DIR / 'model.pkl'
+MODEL_PATH = MODEL_DIR / 'model.pkl'
 
 # Comprobación de directorios y archivos
 if not DATA_DIR.exists():
@@ -17,5 +17,5 @@ if not MODEL_DIR.exists():
     print(f"Advertencia: El directorio de modelos {MODEL_DIR} no existe.")
 if not RAW_DATA_DIR.exists():
     print(f"Advertencia: El archivo de datos {RAW_DATA_DIR} no existe.")
-if not XGB_MODEL_DIR.exists():
-    print(f"Advertencia: El archivo del modelo {XGB_MODEL_DIR} no existe.")
+if not MODEL_PATH.exists():
+    print(f"Advertencia: El archivo del modelo {MODEL_PATH} no existe.")
